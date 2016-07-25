32cb92d49445a8081b43a5490df8b9812c3c8989
Hao Yu <hao.yu.019@gmail.com>
2016-07-24 20:54:04 -0400

    Update helper.h

--- helper.h
+++ helper.h
@@ -1,20 +1,22 @@
 #pragma once
 
-#ifndef Mydownload___
-#define Mydownload___
+#ifndef downloadhelper___
+#define downloadhelper___
 
-#include "stdafx.h"
+#include <afxsock.h>
+#define MAX_RECV_LEN           100   // max length of the string received.
+#define MAX_PENDING_CONNECTS   4     // The maximum length of the queue of pending connections.
 
 class  CHttpSect
 {
 public:
-	CString  szHostAddr;      // Host address
-	int      nHostPort;       // Host port
-	CString  szHttpAddr;      // Http file address
-	CString  szHttpFilename;  // Http filename
-	CString  szDesFilename;   // downloaded filename
-	DWORD    nStart;          // start of segment
-	DWORD    nEnd;            // end of segment
+	CString  szHostAddr;      // Host address.
+	int      nHostPort;       // Host port.
+	CString  szHttpAddr;      // Http file address.
+	CString  szHttpFilename;  // Http file name.
+	CString  szDesFilename;   // Downloaded file name.
+	DWORD    nStart;          // Downloading start point.
+	DWORD    nEnd;            // Downloading end point. 
 };
 
 class  CHttpGet
@@ -25,6 +27,8 @@ public:
 
 private:
 	CHttpSect *sectinfo;
+	static int m_nCount;
+	static UINT ThreadDownLoad(void* pParam);
 
 public:
 	static DWORD m_nFileLength;
@@ -32,10 +36,11 @@ public:
 private:
 	static SOCKET ConnectHttp(CString strHostAddr, int nPort);
 	static BOOL SendHttpHeader(SOCKET hSocket, CString strHostAddr,
-		CString strHttpAddr, CString strHttpFilename, DWORD nPos);
+		CString strHttpAddr, CString strHttpFilename, DWORD nPos, DWORD nEnd);
 	static DWORD GetHttpHeader(SOCKET sckDest, char *str);
 	static BOOL SocketSend(SOCKET sckDest, CString szHttp);
 	static DWORD GetFileLength(char *httpHeader);
+	BOOL FileCombine(CHttpSect *pInfo, FILE *fpwrite);
 
 public:
 	BOOL HttpDownLoad(
@@ -43,7 +48,7 @@ public:
 		CString strHttpAddr,
 		CString strHttpFilename,
 		CString strWriteFileName,
-		// int nSectNum,
+		int nSectNum,
 		DWORD &totalSize);
 };
 
@@ -60,14 +65,30 @@ public:
 	CString GetResponse(SOCKET hSock);
 };
 
+
+class CMyFile
+{
+public:
+	CMyFile();
+	virtual ~CMyFile();
+
+public:
+	BOOL FileExists(LPCTSTR lpszFileName);
+	FILE* GetFilePointer(LPCTSTR lpszFileName);
+	DWORD GetFileSizeByName(LPCTSTR lpszFileName);
+	CString GetShortFileName(LPCSTR lpszFullPathName);
+};
+
+extern CMyFile myfile;
+
 extern CDealSocket dealsocket;
 
 
-bool MyDownload(CString strUrl,
+bool DownloadHelper(CString strUrl,
 	CString strWriteFileName,
-	//unsigned long *& downloaded,
-	unsigned long & totalSize
-//	int nThread = 5
-	);
+	unsigned long *& downloaded,
+	unsigned long & totalSize,
+	int nThread = 5
+);
 
 #endif
